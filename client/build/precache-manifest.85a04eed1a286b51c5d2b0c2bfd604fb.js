0f58a6f982eed44f1f408ffef0262f299f702aa6
kos602 <kalmurzaev.oljas@gmail.com>
2020-12-05 01:42:28 -0500

    initial commit

---
+++ client/build/precache-manifest.85a04eed1a286b51c5d2b0c2bfd604fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17fd703661b1d49b49d8e96b1aa1fe99",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1bba4d977970e54834e0",
+    "url": "/static/js/2.514b395c.chunk.js"
+  },
+  {
+    "revision": "482308fce8537f1b17141b6527a207c9",
+    "url": "/static/js/2.514b395c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed1dc5a1f40d97f1b547",
+    "url": "/static/js/main.22b4ef3a.chunk.js"
+  },
+  {
+    "revision": "8f62c64a0ccc5f387d32",
+    "url": "/static/js/runtime-main.d9f235a9.js"
+  },
+  {
+    "revision": "0e56efd0a097beb41d6d8543df276ac6",
+    "url": "/static/media/google-logo.0e56efd0.svg"
+  }
+]);
